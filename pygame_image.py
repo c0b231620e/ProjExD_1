f242a13954ac3cffba9233e5452168d9aa3f2fa6
yutaka <c0b231620e@edu.teu.ac.jp>
2024-06-11 17:20:19 +0900

    後ろに引きずられるまで

--- pygame_image.py
+++ pygame_image.py
@@ -1,34 +1,42 @@
 import os
 import sys
 import pygame as pg
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    koka = pg.image.load("fig/3.png")
-    koka=pg.transform.flip(koka,True,False)
-    bg_img2=pg.transform.flip(bg_img,True,False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # こうかとんRectの抽出
+    kk_rct.center = 300, 200    
     tmr = 0
-    x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        x=tmr%3200
-        screen.blit(bg_img, [-x,0])
-        screen.blit(bg_img2, [-x+1600,0])
-        screen.blit(bg_img, [-x+3200,0])
-        screen.blit(bg_img2, [-x+4800,0])
-        kk=koka.get_rect()##場所取得
-        ba=kk.center =300,200
-        screen.blit(koka, ba)##baのところに描画
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])        
+
+        key_lst = pg.key.get_pressed()  # 全キーの押下状態を取得
+        kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+            kk_rct.move_ip(2, 0)
+        screen.blit(kk_img, kk_rct)  # kk_imgをkk_rctの設定にしたがって貼り付け
+
+
         pg.display.update()
-        tmr += 1        
+        tmr += 1      
         clock.tick(200)
 
 
