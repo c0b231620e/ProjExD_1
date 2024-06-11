8220bd2fd560e6b90820cb38f9711877f9720767
yutaka <c0b231620e@edu.teu.ac.jp>
2024-06-11 16:32:02 +0900

    ワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -12,12 +12,18 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     koka = pg.image.load("fig/3.png")
     koka=pg.transform.flip(koka,True,False)
+    bg_img2=pg.transform.flip(bg_img,True,False)
     tmr = 0
+    x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        
+        x=tmr%3200
+        screen.blit(bg_img, [-x,0])
+        screen.blit(bg_img2, [-x+1600,0])
+        screen.blit(bg_img, [-x+3200,0])
+        screen.blit(bg_img2, [-x+4800,0])
         kk=koka.get_rect()##場所取得
         ba=kk.center =300,200
         screen.blit(koka, ba)##baのところに描画
