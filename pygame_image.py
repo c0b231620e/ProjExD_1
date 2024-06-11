20b656e23886556379fc334cb442b7f9bebf374d
yutaka <c0b231620e@edu.teu.ac.jp>
2024-06-11 15:57:33 +0900

    問題を3門目まで

---
+++ pygame_image.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    koka = pg.image.load("fig/3.png")
+    koka=pg.transform.flip(koka,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(koka, [300, 200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
